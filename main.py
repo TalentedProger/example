15b6e9418e3a837fae54c434edd8eeeefea5fcc3
Salim Sokurov <lasonandy@gmail.com>
2025-07-11 21:00:09 +0300

    123

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from setup import *
 
 import os
 TOKEN = os.getenv("TOKEN")
